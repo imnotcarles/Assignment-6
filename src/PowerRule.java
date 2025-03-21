73b4abbd642dc40173307eddf5350ef164b5853d
Huang <Charles.Huang2@student.tdsb.on.ca>
2025-03-21 11:26:25 -0400

    First code for Assignment 6

---
+++ src/PowerRule.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class PowerRule {
+    public static void main(String[] args) throws Exception {
+        Scanner keyboard = new Scanner(System.in);
+
+
+        System.out.println("Please enter the coefficients:");
+        String coefficients = keyboard.nextLine();
+        System.out.println("Please enter the degrees:");
+        String degrees = keyboard.nextLine();
+        keyboard.close();
+    }
+}
